f2b534b558b3e1692b89abeede3868dd982ec3d7
1samson1 <ya.samsono2013@ya.ru>
2021-08-16 20:52:24 +0300

    add new modules

--- engine/admin/data/modules.php
+++ engine/admin/data/modules.php
@@ -30,5 +30,23 @@
             "image" => "skin/img/icon.png",
             "description" => "Создание и редактирование новостей на сайте",
         ),
+        array(
+            "name" => "categories",
+            "verbose_name" => "Категории товаров",
+            "image" => "skin/img/icon.png",
+            "description" => "Управление категорияси товаров , создание, редактирование и удаление",
+        ),
+        array(
+            "name" => "tovars",
+            "verbose_name" => "Товары",
+            "image" => "skin/img/icon.png",
+            "description" => "Добавление, редактирование и удаление товаров представленных на сайте",
+        ),
+        array(
+            "name" => "orders",
+            "verbose_name" => "Заказы",
+            "image" => "skin/img/icon.png",
+            "description" => "Просмотр заказов пользователей и выполнение над ними действий",
+        ),
     );
 ?>
